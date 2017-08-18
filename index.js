7480d81e2f5e859f15b231ab44eee9b1b7fd5389
Paul <paulr@freelearn.tv>
2017-08-19 09:53:19 +1200

    Add google config to keys.js and add keys.js to .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-//express app root route '/' handler
+//tell passport to use a new GoogleStrategy() constructor
+
+passport.use(new GoogleStrategy());
+
 app.get('/', (req, res) => {
   res.send({ hi: 'twitter #gotta.luv #code' });
 });
