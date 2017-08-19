5fadffcfdb793c934c2e0dfd2fdd16e8c5825b53
Paul <paulr@freelearn.tv>
2017-08-20 09:32:34 +1200

    Install mongoose and connect to mlab instance of mongodb

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongodbURI);
 
 const app = express();
 
