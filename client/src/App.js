695e2c837819ba8fe54b5809c96a9bcd769562eb
Paul <paulr@freelearn.tv>
2017-08-21 06:55:24 +1200

    npm install --save concurrently and update package.json

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>#twitter #gotta.luv.react</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">click to login</p>
       </div>
     );
   }
