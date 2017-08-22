f4c676703f7a3a1908046f6a48feed9ffee55d56
Paul <paulr@freelearn.tv>
2017-08-23 06:29:51 +1200

    npm install --save materialize-css and add materialize.css to index.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            WePlay
+          </a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
