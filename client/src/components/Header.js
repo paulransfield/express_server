6368c2f40d7b706b73906813a255941f06f3158d
Paul <paulr@freelearn.tv>
2017-08-23 14:20:26 +1200

    Switch case check for auth.props to in renderContent helper method

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/google">Login</a>;
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="left brand-logo">
+          <a href="/" className="left brand-logo">
             WePlay
           </a>
           <ul id="nav-mobile" className="right">
             <li>
-              <a href="#">About us</a>
+              <a href="/">About us</a>
             </li>
             <li>
-              <a href="#">Login</a>
+              {this.renderContent()}
             </li>
           </ul>
         </div>
@@ -22,4 +34,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
