f2e5fc463e29d33b7d31941c15613aafff8140c1
Paul <paulr@freelearn.tv>
2017-08-24 06:06:29 +1200

    Add body-parser middleware and add stripe.charges.create

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer-copyright red darken-4">
       <ul>
-        <li>© 2014 Copyright Text</li>
+        <li>2017</li>
       </ul>
     </footer>
   );
