ca18cb9e6b1d739620be8c20e911fe35a6d9ce8a
Paul <paulr@freelearn.tv>
2017-08-23 13:12:11 +1200

    Add fetchUser action creator

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+//create action creator
+
 //first argument = redux action reducers
 //second argument = initial state {} for server side rendering
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
