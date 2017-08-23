2e6cb5f649bbe665f8a8dae4eecb3d799864371c
Paul <paulr@freelearn.tv>
2017-08-23 18:36:06 +1200

    Link {this.props.user ? '/surveys' : '/'} + react-stripe-checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -17,13 +18,13 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
             WePlay
-          </a>
+          </Link>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="/">About us</a>
-            </li>
             <li>
               {this.renderContent()}
             </li>
